ef7ee77379c24b1212cec21e9f99199285ee352c
albertissac12 <abhiodf9@gmail.com>
2025-10-23 17:15:17 +0000

    Added Abstract contract for state variables

--- Contract/contracts/Counter.sol
+++ Contract/contracts/Counter.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract ZKBioRegistry {
-
+abstract contract StateVars {
   event Commitment(uint256 indexed id,uint256 indexed p_hash);
   error InvalidCommiter();
-
-  mapping(uint256=>uint256) private commitments;
-
+  mapping(uint256=>uint256) internal commitments;
   address public commiter;
 
   modifier hasRole {
     if(msg.sender!=commiter) revert InvalidCommiter();
     _;    
   }
+  constructor(address _commiter) {
+    commiter = _commiter;
+  }
+}
+
+contract ZKBioRegistry is StateVars{
+
+  constructor (address _commiter) StateVars(_commiter) {}
 
   function commit(uint256 id, uint256 p_hash) external hasRole{
     emit Commitment(id,p_hash);
@@ -23,6 +28,5 @@ contract ZKBioRegistry {
   function reterieve(uint256 id) external view returns(uint256){
     return commitments[id];
   }
-
 
 }
